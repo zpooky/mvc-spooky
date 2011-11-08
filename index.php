38b5d02c8f3e11e78359391df89eaac555ef8e11
Fredrik Olsson <hell_trooper@msn.com>
2011-11-08 15:37:41 +0100

    Added models and index base

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+@define('ROOT','../');
+
+if(!isset($_GET['p'])){
+	$_GET['p'] = 'home';
+}
+switch($_GET['p']){
+	case 'home':
+	break;
+	default:
+}
+?>
---
+++ model/BaseModel.php
@@ -0,0 +1,8 @@
+<?php
+abstract class BaseModel {
+	private $id;
+	public getId(){
+		return $id;
+	}
+}
+?>
