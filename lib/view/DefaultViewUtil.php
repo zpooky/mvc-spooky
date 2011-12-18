68e90aa2ac018fd9cad9d6479d12472d29354531
Fredrik Olsson <hell_trooper@msn.com>
2011-12-18 18:37:14 +0100

    m

---
+++ lib/view/DefaultViewUtil.php
@@ -0,0 +1,21 @@
+<?php
+class DefaultViewUtil {
+	public static function getCMSMenu($loggedIn = true,$root){
+		if(!$loggedIn){
+			return '';
+		}
+		$createCMSURL = '<a href="'.$root.'cms/create">Create</a>';
+		$listCMSURL = '<a href="'.$root.'cms/alist">List</a>';
+		return <<<EOD
+<div id="menu">
+	<p style="background-color: #b5b5aa;padding-bottom: 0px;margin-bottom:0px;">CMS Menu</p>
+	<ul style="padding-left: 0px;padding-top: 0px;margin-top:0px;">
+	<li style="display: block;">{$createCMSURL}</li>
+	<li style="display: block;">{$listCMSURL}</li>
+	</p>
+</div>
+EOD;
+	}
+}
+
+?>
