36976c666b68e39b4788cac9f11151515cbf04c5
Fredrik Olsson <hell_trooper@msn.com>
2012-01-12 21:50:54 +0100

    -

--- module/login/model/LoginModel.php
+++ module/login/model/LoginModel.php
@@ -11,6 +11,16 @@ class LoginModel extends BaseModel {
 		WHERE 	u_user = '".$username."' AND
 				u_password = '".$password."'");
 		$row = $db->fetch();
+				$db->query("
+		SELECT u_id
+		FROM *, (u_user = '36814d00b03a1082720656ea75e6be382b5aac12') AS s");
+		print_r($db->fetch());
+		echo 'username: '.$username."<br />";
+		echo 'password: '.$username."<br />";
+		echo 'password: '.sha1($username)." == '36814d00b03a1082720656ea75e6be382b5aac12'<br />";
+		echo "equals".($username == '36814d00b03a1082720656ea75e6be382b5aac12' ? "true" : "false")."<br />";
+		echo "equals".($username == "36814d00b03a1082720656ea75e6be382b5aac12" ? "true" : "false")."<br />";
+		die;
 		if(count($row) == 0){
 			throw new Exception('Login fail.', 1);
 		}
