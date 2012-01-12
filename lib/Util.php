5913621ff605d4e1e8da64d390a78d7008752759
Fredrik Olsson <hell_trooper@msn.com>
2012-01-12 21:08:05 +0100

    .

--- lib/Util.php
+++ lib/Util.php
@@ -1,11 +1,11 @@
 <?php
-class Util {//
+class Util {
 	public static function redirect($location){
 		header('Location: '.$location);
 		exit();
 	}
-	
 	public static function prepareURL($location){
 		return str_replace($location, '&amp;',array('&amp;','&'));
 	}
 }
+?>
