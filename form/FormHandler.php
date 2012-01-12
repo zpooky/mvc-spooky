240abaab110858b9ed0d1edc9f5a6870f1db937f
Fredrik Olsson <hell_trooper@msn.com>
2012-01-12 21:03:54 +0100

    .

--- form/FormHandler.php
+++ form/FormHandler.php
@@ -1,21 +1,1 @@
-<?php
-session_start();
-@define('ROOT','../');
-/**
- * c: class
- * f: function
- * u: URL
- */
-if(!isset($_GET['c']) || !isset($_GET['f'])){
-	die('fail');
-}
-//crappy crap crap
-$url = !isset($_GET['u']) || $_GET['u'] == null ? ROOT.'form/' : ROOT.'module/'.urldecode($_GET['u']).'/form/';
-$class = str_replace(array('/','\\'), '', $_GET['c']);
-$function = $_GET['f'];
-require_once $url.$class.'.php';
-
-$formValidator = new $class;
-$formValidator->$function();
-
-?>
+<?phpsession_start();@define('ROOT','../');/*** c: class* f: function* u: URL*/if(!isset($_GET['c']) || !isset($_GET['f'])){	die('fail');}$url = !isset($_GET['u']) || $_GET['u'] == null ? ROOT.'form/' : ROOT.'module/'.urldecode($_GET['u']).'/form/';$class = str_replace(array('/','\\'), '', $_GET['c']);$function = $_GET['f'];require_once $url.$class.'.php';$formValidator = new $class;$formValidator->$function();?>
