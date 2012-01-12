e8d4bc879a8bf38b7c7c192b9002f80ccdc0c323
Fredrik Olsson <hell_trooper@msn.com>
2012-01-12 23:26:01 +0100

    -

--- module/login/model/LoginModel.php
+++ module/login/model/LoginModel.php
@@ -7,32 +7,33 @@ class LoginModel extends BaseModel {
 		$username = $db->escape($username);
 		$db->query("
 		SELECT u_id, u_password, u_user
-		FROM user
-		WHERE (u_user = '".$username."' || u_user = '".utf8_decode($username)."' || u_user = '".utf8_encode($username)."') ");
-		$row = $db->fetch();
-		
-		$db->query("
-		SELECT *, (u_password = '36814d00b03a1082720656ea75e6be382b5aac12')AS o, (u_password = '36814d00b03a1082720656ea75e6be382b5aac12') AS s, (u_password = '".$password."') AS b
 		FROM user");
-		print_r($db->fetch());
-		echo 'username: '.$username."<br />";
-		echo 'password: '.$password."<br />";
-		echo 'password: '.$password." == '36814d00b03a1082720656ea75e6be382b5aac12'<br />";
-		echo "equals".($password == '36814d00b03a1082720656ea75e6be382b5aac12' ? "true" : "false")."<br />";
-		echo "equals".($password == "36814d00b03a1082720656ea75e6be382b5aac12" ? "true" : "false")."<br />";
-		echo "equals".($password == utf8_decode('36814d00b03a1082720656ea75e6be382b5aac12') ? "true" : "false")."<br />";
-		echo "equals".($password == utf8_encode("36814d00b03a1082720656ea75e6be382b5aac12") ? "true" : "false")."<br />";
-		echo "equals".(utf8_decode($password) == utf8_decode('36814d00b03a1082720656ea75e6be382b5aac12') ? "true" : "false")."<br />";
-		echo "equals".(utf8_encode($password) == utf8_encode("36814d00b03a1082720656ea75e6be382b5aac12") ? "true" : "false")."<br />";
-		echo "equals".(utf8_decode($password) == utf8_decode('36814d00b03a1082720656ea75e6be382b5aac12') ? "true" : "false")."<br />";
-		echo "equals".(utf8_decode($password) == utf8_encode("36814d00b03a1082720656ea75e6be382b5aac12") ? "true" : "false")."<br />";
-		echo "rows: ".count($row)."<br />";
-		echo "row  pw:".$row[0]['u_password']."<br />";
-		if(count($row) == 0 || $row[0]['u_password'] != $password){
-			die("login fail");
-			throw new Exception('Login fail.', 1);
+		$row = $db->fetch();
+		foreach($row as &$bad){
+			if($bad['u_user'] == $username && $bad['u_password'] == $password){
+				return $bad['u_id'];
+			}
 		}
-		return $row[0]['u_id'];
+		/*
+		 $db->query("
+		 SELECT *, (u_password = '36814d00b03a1082720656ea75e6be382b5aac12')AS o, (u_password = '36814d00b03a1082720656ea75e6be382b5aac12') AS s, (u_password = '".$password."') AS b
+		 FROM user");
+		 print_r($db->fetch());
+		 echo 'username: '.$username."<br />";
+		 echo 'password: '.$password."<br />";
+		 echo 'password: '.$password." == '36814d00b03a1082720656ea75e6be382b5aac12'<br />";
+		 echo "equals".($password == '36814d00b03a1082720656ea75e6be382b5aac12' ? "true" : "false")."<br />";
+		 echo "equals".($password == "36814d00b03a1082720656ea75e6be382b5aac12" ? "true" : "false")."<br />";
+		 echo "equals".($password == utf8_decode('36814d00b03a1082720656ea75e6be382b5aac12') ? "true" : "false")."<br />";
+		 echo "equals".($password == utf8_encode("36814d00b03a1082720656ea75e6be382b5aac12") ? "true" : "false")."<br />";
+		 echo "equals".(utf8_decode($password) == utf8_decode('36814d00b03a1082720656ea75e6be382b5aac12') ? "true" : "false")."<br />";
+		 echo "equals".(utf8_encode($password) == utf8_encode("36814d00b03a1082720656ea75e6be382b5aac12") ? "true" : "false")."<br />";
+		 echo "equals".(utf8_decode($password) == utf8_decode('36814d00b03a1082720656ea75e6be382b5aac12') ? "true" : "false")."<br />";
+		 echo "equals".(utf8_decode($password) == utf8_encode("36814d00b03a1082720656ea75e6be382b5aac12") ? "true" : "false")."<br />";
+		 echo "rows: ".count($row)."<br />";
+		 echo "row  pw:".$row[0]['u_password']."<br />";*/
+		throw new Exception('Login fail.', 1);
+
 	}
 }
 ?>
